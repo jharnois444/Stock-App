7882cc841ebe24d7196640457c97e6637ddda256
jharnois444 <jharnois444@gmail.com>
2019-06-20 21:00:29 -0700

    added devise gem for user login

--- Stock-App/app/controllers/welcome_controller.rb
+++ Stock-App/app/controllers/welcome_controller.rb
@@ -1,12 +1,17 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:ticker]
+    if params[:ticker] == ""
+      @nothing = "Please enter a ticker symbol"
+    elsif params[:ticker]
        @stock = StockQuote::Stock.quote(params[:ticker]) 
+       if !@stock
+          @error = "That stock symbol doesn't exist. Please enter a valid symbol."
+       end
     end
     
   end
 
-    def about
-    end
+  def about
+  end
 
 end
