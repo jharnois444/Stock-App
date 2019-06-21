b851321ecf50fbf0aff63eebd9ea32a95298fbf0
jharnois444 <jharnois444@gmail.com>
2019-06-20 18:54:05 -0700

    added stock lookup form

--- Stock-App/app/controllers/welcome_controller.rb
+++ Stock-App/app/controllers/welcome_controller.rb
@@ -1,9 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+    if params[:ticker]
+       @stock = StockQuote::Stock.quote(params[:ticker]) 
+    end
+    
   end
 
     def about
     end
-
 
 end
